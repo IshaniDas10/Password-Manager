be66fd3ebe0f523b8903b93c480551cc61b4f92e
Ishani das <24157020@kiit.ac.in>
2026-07-17 10:45:47 +0530

    Prepare backend for deployment

--- server/index.js
+++ server/index.js
@@ -5,10 +5,12 @@ const cors = require("cors");
 const app = express();
 
 // Allow requests from the frontend
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 // Allow server to read JSON data
 app.use(express.json());
@@ -23,10 +25,10 @@ app.get("/", (req, res) => {
   res.send("Password Manager Backend Running");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
 //registering route
 const passwordRoutes = require("./routes/passwordRoutes");
 app.use("/api/passwords", passwordRoutes);
